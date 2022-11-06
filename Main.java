3021b21fea419b300baef96c2ba5033998960192
blgsn <111683241+blgsn@users.noreply.github.com>
2022-11-06 21:59:36 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
 
-
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
+		//assign the minimum to the maximum positive integer value that can be represented in 32 bits
 		 int min = Integer.MAX_VALUE;
+		 //assign the maximum to represents the minimum or least integer value that can be represented in 32 bits
 		    int max = Integer.MIN_VALUE;
-
+	//asks the user how many values to enter and gets the value
 	 Scanner input = new Scanner(System.in);
      System.out.print("kaç tane sayı gireceksiniz ?");
      int number = input.nextInt();
-    
+    //shows numbers,if the number smaller than assigned min value,the number is min
+     //if the number greater than assigned max value,the number is max
      for(int i=1;i<=number;i++) {
     	 System.out.print(i+".sayıyı giriniz.");
          int n = input.nextInt(); 
@@ -21,6 +23,7 @@ public class Main {
             max = n;
          }
      }
+     //prints the max and min values 
        System.out.println("minimum :"+min);
      System.out.println("maximum:" +max);
    
